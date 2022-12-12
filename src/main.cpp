ae4a655f2e57b24e5260d96caf0131abda863fcf
Seth-McKilla <seth@endevrs.dev>
2022-12-12 16:18:34 -0500

    Simply blinking light

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#define onboard 13
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(onboard, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(onboard, HIGH);
+  delay(1000);
+  digitalWrite(onboard, LOW);
+  delay(1000);
 }
