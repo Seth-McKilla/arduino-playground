31e2cb8263b0899b64a9dd6992e61ede2b324b3d
Seth-McKilla <seth@endevrs.dev>
2022-12-13 11:32:59 -0500

    Multi-colored LED blinking

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <Arduino.h>
-#define onboard 13
+#define onboard_red 8
+#define onboard_blue 9
+#define onboard_green 10
+#define delay_time delay(500)
 
 void setup() {
-  pinMode(onboard, OUTPUT);
+  pinMode(onboard_red, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(onboard, HIGH);
-  delay(1000);
-  digitalWrite(onboard, LOW);
-  delay(1000);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(onboard_red, HIGH);
+    delay_time;
+    digitalWrite(onboard_red, LOW);
+    delay_time;
+  }
+
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(onboard_blue, HIGH);
+    delay_time;
+    digitalWrite(onboard_blue, LOW);
+    delay_time;
+  }
+
+  for (int i = 0; i < 15; i++) {
+    digitalWrite(onboard_green, HIGH);
+    delay_time;
+    digitalWrite(onboard_green, LOW);
+    delay_time;
+  }
 }
